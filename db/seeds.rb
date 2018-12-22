967f3fb627a531c5477c44224543bac70f55cc28
Brady <vanderbannond@gmail.com>
2018-12-21 17:39:42 -0800

    add seeds, returning paginated json to frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |i|
+  Article.create({title: "Article title #{i}", content: "Article content #{i}", slug: "Article slug #{i}"})
+end
