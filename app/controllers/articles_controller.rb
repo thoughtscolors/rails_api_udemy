565d88c4e8eb0dc7d90122df34a37dfb16c512b5
Brady <vanderbannond@gmail.com>
2018-12-21 17:27:15 -0800

    add kaminari pagination, tests passing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
   def index
     articles = Article.recent
+      .page(params[:page])
+      .per(params[:per_page])
     render json: articles
   end
 
