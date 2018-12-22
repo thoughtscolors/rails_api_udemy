0f1c408fb0a0f4234a134f2b8b1682c7d9d7d5a5
Brady <vanderbannond@gmail.com>
2018-12-21 17:05:27 -0800

    add ordering articles descending, tests passing

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+
   describe "#validations" do
+
     it 'should test that the factory is valid' do
       expect(build :article).to be_valid
     end
@@ -30,4 +32,19 @@ RSpec.describe Article, type: :model do
       expect(invalid_article).not_to be_valid
     end
   end
+
+  describe '.recent' do
+    it 'should list most recent article first' do
+      old_article = create :article
+      new_article = create :article
+      expect(described_class.recent).to eq(
+        [new_article, old_article]
+      )
+      old_article.update_column :created_at, Time.now
+      expect(described_class.recent).to eq(
+        [old_article, new_article]
+      )
+    end
+  end
+
 end
