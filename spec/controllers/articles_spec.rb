010e109a888a6283113018cef480073d4cee89ad
Brady <vanderbannond@gmail.com>
2018-12-21 16:23:34 -0800

    refactor test to use json helpers and subect variable, passing

--- spec/controllers/articles_spec.rb
+++ spec/controllers/articles_spec.rb
@@ -4,16 +4,16 @@ describe ArticlesController do
 
   describe '#index' do
 
+    subject { get :index }
+
     it 'should return reponse success code' do
-      get :index
+      subject
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return proper json' do
       articles = create_list :article, 2
-      get :index
-      json = JSON.parse(response.body)
-      json_data = json['data']
+      subject
       articles.each_with_index do |article,index|
         expect(json_data[index]['attributes']).to eq({
           "title" => article.title,
