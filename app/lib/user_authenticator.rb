f358a895e15cfeb68ed88a0bd10af407e5e3d9bb
Brady <vanderbannond@gmail.com>
2018-12-25 13:51:36 -0800

    add user authenticator, test passing

---
+++ app/lib/user_authenticator.rb
@@ -0,0 +1,15 @@
+class UserAuthenticator
+
+  class AuthenticationError < StandardError; end
+
+  attr_reader :user
+
+  def initialize(code)
+
+  end
+
+  def perform
+    raise AuthenticationError
+  end
+
+end
