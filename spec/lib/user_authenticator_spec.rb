ed472ff1551949b1415f4c2ad729114c0b606ca5
Brady <vanderbannond@gmail.com>
2018-12-25 13:59:47 -0800

    add correct test, currently red

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -4,17 +4,24 @@ describe UserAuthenticator do
 
   describe '#perform' do
 
-    context 'when code is incorrect' do
+    let(:authenticator) { described_class.new('sample_code') }
+    subject { authenticator.perform }
 
+    context 'when code is incorrect' do
       it 'should raise an error' do
-        authenticator = described_class.new('sample_code')
-        expect{authenticator.perform}.to raise_error(
+        expect{ subject }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
       end
-      
     end
+
+    context 'when code is correct' do
+      it 'should save new user when user does not exist' do
+        expect{ subject }.to change{ User.count }.by(1)
+      end
+    end
+
   end
 
 end
