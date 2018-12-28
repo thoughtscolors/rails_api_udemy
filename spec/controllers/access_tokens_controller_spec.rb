8a3363d30cba519a2f74acf1b598c6fa7739d0a8
Brady <vanderbannond@gmail.com>
2018-12-27 17:16:30 -0800

    add failing test

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
+  describe '#create' do
+    context 'when invalid request' do
+      it 'should return 401 status code' do
+        post :create
+        expect(response).to have_http_status_code(401)
+      end
+    end
 
+    context 'when successful request' do
+
+    end
+  end
 end
