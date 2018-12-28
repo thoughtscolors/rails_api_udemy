195e2ada81dc8f30176ed70f2cdd8b9e420946fb
Brady <vanderbannond@gmail.com>
2018-12-27 17:13:10 -0800

    add route, controller, and test for login, passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+  post 'login', to: 'access_tokens#create'
 end
