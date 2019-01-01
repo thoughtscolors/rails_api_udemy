84abf066f01c49800a986537bf0a699ad622f8f2
Brady <vanderbannond@gmail.com>
2018-12-31 16:49:54 -0800

    finish standard auth, all controller tests passing

--- app/lib/user_authenticator/standard.rb
+++ app/lib/user_authenticator/standard.rb
@@ -1,6 +1,12 @@
 class UserAuthenticator::Standard < UserAuthenticator
   class AuthenticationError < StandardError; end
 
-  
+  def initialize(login: nil, password: nil)
+
+  end
+
+  def perform
+    raise AuthenticationError
+  end
 
 end
