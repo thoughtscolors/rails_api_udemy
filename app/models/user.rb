31f3bc630be319b51a62ac8828a5e81296017a85
Brady <vanderbannond@gmail.com>
2018-12-31 17:43:07 -0800

    add encrypted pass to model, passing two standard auth tests now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
+  include BCrypt
+
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
 
   has_one :access_token, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def password
+    @password ||= Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.encrypted_password = @password
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_30_043212) do
+ActiveRecord::Schema.define(version: 2019_01_01_013821) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.string "token", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_12_30_043212) do
     t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password"
   end
 
 end
