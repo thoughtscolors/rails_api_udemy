f5be57d3627ea904dbbe213ff6208caf30b44fa3
Brady <vanderbannond@gmail.com>
2018-12-31 21:50:01 -0800

    add logic to validate password is present, passing tests, postman registration works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,21 @@ class User < ApplicationRecord
 
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
+  validates :password, presence: true, if: -> { provider == 'standard' }
 
   has_one :access_token, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   def password
+    # including BCrypt allows Password class to be referenced directly, otherwise it would be BCrypt::Password
     @password ||= Password.new(encrypted_password) if encrypted_password.present?
   end
 
   def password=(new_password)
+    #this handles if password is nil, otherwise BCrypt would still hash
+    return @password = new_password if new_password.blank?
+
     @password = Password.create(new_password)
     self.encrypted_password = @password
   end
