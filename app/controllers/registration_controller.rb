3f9a4e305f10173a288698471972c803e4eafbd9
Brady <vanderbannond@gmail.com>
2018-12-31 21:33:20 -0800

    add error handling, passing 4 of 5 tests

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,6 +5,10 @@ class RegistrationController < ApplicationController
     user = User.new(registration_params.merge(provider: 'standard'))
     user.save!
     render json: user, status: :created
+  rescue ActiveRecord::RecordInvalid
+    render json: user, adapter: :json_api,
+      serializer: ErrorSerializer,
+      status: :unprocessable_entity
   end
 
   private
